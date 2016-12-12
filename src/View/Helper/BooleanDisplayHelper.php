742235bc43af378855a242fec2597ce89e99719c
testaj <jared.testa@nifa.aero>
2016-12-12 11:00:21 -0800

    added-methods-boolean-display

--- src/View/Helper/BooleanDisplayHelper.php
+++ src/View/Helper/BooleanDisplayHelper.php
@@ -7,9 +7,34 @@ use Cake\View\Helper;
 class BooleanDisplayHelper extends Helper
 {
 
+	//left for compatibility
     public function format($value, $showNo = false) {
         $falseResponse = ($showNo) ? "No" : "";
         return ($value) ? "Yes" : $falseResponse ;
+    }
+
+        /**
+     * yesNo method
+     *
+     * @param boolean| $value boolean
+     * @return string
+     *
+     */
+
+    public function yesNo($value, $showNo = true) {
+        return ($value) ? "Yes" : (($showNo) ? "No" : "");
+    }
+
+    /**
+     * trueFalse method
+     *
+     * @param boolean| $value boolean
+     * @return string
+     *
+     */
+
+    public function trueFalse($value, $showFalse = true) {
+        return ($value) ? "True" : (($showFalse) ? "False" : "");
     }
 
 }
