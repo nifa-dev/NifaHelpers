ccb8be3902e341eb77fbc6e2a1cc96e98a3be2d9
testaj <jared.testa@nifa.aero>
2016-12-14 11:09:02 -0800

    environment-component

---
+++ src/Controller/Component/EnvironmentComponent.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller\Component;
+
+use Cake\Controller\Component;
+use Cake\Network\Request;
+use Cake\Event\Event;
+
+class EnvironmentComponent extends Component
+{
+
+
+    public function categorize($environment) {
+        if(in_array(strtolower($environment), ['development', 'training', 'production'])) {
+            return strtolower($environment);
+        }
+
+        return false;
+    }
+
+
+    public function beforeRender(Event $event) {
+        $controller = $event->subject();
+        $controller->set('_environment', $this->categorize(env('ENVIRONMENT_TYPE')));
+
+    }
+
+
+}
