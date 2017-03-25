af81076fa470e2457e5bbbc10271c6b6c97753ef
testaj <jared.testa@nifa.aero>
2017-03-25 07:51:23 -0700

    lookup-field
    Added “ Number” to lookup field label.

--- src/View/Helper/ParticipantLookupHelper.php
+++ src/View/Helper/ParticipantLookupHelper.php
@@ -15,7 +15,7 @@ class ParticipantLookupHelper extends Helper
         $fieldId = str_replace(".", "-", $field);
         $auto = ($autofocus == true) ? "autofocus" : "";
         $html = $this->_View->element('NifaHelpers.participant_lookup_js', ['fieldId' => $fieldId, 'url' => 'participants/lookup', 'contestId' => $contestId]);
-        $html.= $this->Form->input($field . "_lookup", ['type' => 'text', 'label' => $label, 'id' => $fieldId . "-lookup", 'autofocus' => $auto]);
+        $html.= $this->Form->input($field . "_lookup", ['type' => 'text', 'label' => $label . " Number", 'id' => $fieldId . "-lookup", 'autofocus' => $auto]);
         $html.= $this->Form->hidden($field . "_id", ['type' => 'text', 'label' => $label . " ID", 'id' => $fieldId]);
         $html.= $this->Form->input($field . "_name", ['label' => $label . " Name", 'id' => $fieldId . "-name", 'readonly' => 'readonly']);
         $html.= $this->Form->input($field . "_team", ['label' => $label . " Team", 'id' => $fieldId . "-team", 'readonly' => 'readonly']);
